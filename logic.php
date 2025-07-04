13746f4e2cef7d6d4e3d3ad8e6126b8d791e13d0
keanehatescoding <keanembae@gmail.com>
2025-07-04 09:03:32 +0300

    feat: added nice css background

--- logic.php
+++ logic.php
@@ -1,14 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+	<link href="styles.css" rel="stylesheet">
+</head>
+<body>
 <?php
 require_once "Icons.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	if (!isset($_POST["city"]) || empty(trim($_POST["city"]))) {
-		die('<div id="city-warning" class="alert alert-danger" role="alert"><p>You must enter a city</p></div>');
+		$error = "You must enter a city";
+		error_log("[" . date("Y-m-d H:i:s") . "] " . $error);
+		die('<div id="city-warning" class="alert alert-danger text-center" role="alert"><p><b>' . $error . '</b></p></div>');
 	}
 	$city = trim($_POST["city"]);
 
 	// if this finds an illegal character then it dies immediately to avoid sending malicious input therefore preventing XSS 
 	if (!preg_match("/^[\p{L}\p{M}\s\.\'\-\(\)]{2,100}$/u", $city)) {
-		die('<div id="city-warning" class-"alert alert-danger" role="alert"><p>Invalid City name, it contains illegal characters</div>');
+		$error  =  "Inavlid city name, it contains illegal characters";
+		error_log("[" . date("Y-m-d H:i:s") . "] " . $error);
+		die('<div id="city-warning" class-"alert alert-danger" role="alert"><p><b>' . $error . '</div>');
 	}
 
 	// Basic rate limiting, OpenWeatherMap API allows a maximum of 1000 request per day on the free tier, we therefore have to limit the number of requests to 15 for each user per day.
@@ -33,7 +46,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	});
 	if (count($requests) >=$limit){
 		http_response_code(429);
-		die("We only allow 15 requests per day. Try again later");
+		error_log('[' . date("Y-m-d H:i:s") . "]" . $_SESSION["REMOTE_ADDR"] . ' has reached maximum number of requests ');
+		die("We only allow 15 requests per day. Try again tommorow");
 	}
 	$requests[] = $now;
 	# XSS protection via input sanitzation
@@ -58,10 +72,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			throw new Exception("Invalid weather data received from API");
 		}
 
-		$weather = '<b>'. htmlspecialchars($weather_Array['name']) . ", " . htmlspecialchars($weather_Array['sys']['country']) . '</b><br>';
+		$weather = '<div class="alert alert-success text-center" role="alert" style="text-align:center; justify-content: center; align-items: center>"';
+		$weather .= '<b>'. htmlspecialchars($weather_Array['name']) . ", " . htmlspecialchars($weather_Array['sys']['country']) . '</b><br>';
 		$weather .= 'Temperature: <b>' . intval($weather_Array['main']['temp']) . '°C</b><br>';
 		$weather .= 'Humidity: <b>' . intval($weather_Array['main']['humidity']) . '%</b><br>';
-		$weather .= 'Weather Condtion: <b>' . htmlspecialchars($weather_Array['weather']['0']['description'])  . '</b><br>';
+		$weather .= 'Weather Condition: <b>' . htmlspecialchars($weather_Array['weather']['0']['description'])  . '</b><br>';
 		$weather .= 'Atmospheric Pressure <b>' . htmlspecialchars($weather_Array['main']['pressure'])  . ' hPa</b><br>';
 		$weather .= 'Icon: ' . htmlspecialchars(Icons::get($weather_Array['weather']['0']['icon'])) . '<br>';
 
@@ -72,16 +87,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$weather .= "Sunrise: <b>" . date("g:i a", $sunrise) . '</b><br>';
 		$weather .= "Sunset: <b>" . date("g:i a", $sunset) . '</b><br>';
 		$weather .= "Current Time: <b>" . date("F j, Y, g:i a") . '</b><br>';
+		$weather .= '</div>';
 
-		$_SESSION["weather_result"] = $weather;
-		header("Location: index.php");
-		exit;
+		echo $weather;
+
 	} catch(Exception $e) {
-		$error = '<div id="city-warning" class="alert alert-danger" role=alert"><p>Error occured: <b>' . htmlspecialchars($e->getMessage()) . ' </b><br></div>';
-		die($error);
+		$error =  htmlspecialchars($e->getMessage);
+		error_log("[" . date("Y-m-d H:i:s") . "] " . $error);
+		die('<div id="city-warning" class="alert alert-danger" role=alert"><p>Error occured: <b>' . $error  . ' </b><br></div>');
 	}
 } else {
-	$error =  '<div class="alert alert-danger" role="alert"><p>You must enter a city</p></div>';
-	echo $error;
+	$error = 'Method not allowed';
+	error_log("[" . date("Y-m-d H:i:s") . "] " . $error);
+	die('<div class="alert alert-danger" role="alert"><p><b>' . $error . '</b></p></div>');
 }
 ?>
+</body>
+</html>
